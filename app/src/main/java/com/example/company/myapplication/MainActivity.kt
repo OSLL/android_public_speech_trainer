8fc8ecba5979d4bbe88505b9a8345d5e5a1e9db9
Evgeny Motorin <zmotorin174@gmail.com>
2018-08-20 19:37:52 +0500

    fix mainactivity

--- app/src/main/java/com/example/company/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.company.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
@@ -8,9 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        val i = Intent(this, GetTextForPresentation::class.java)
-        startActivity(i)
     }
 }
