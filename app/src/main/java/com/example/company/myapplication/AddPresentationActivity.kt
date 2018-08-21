0347f1102fb7286e7743f5b21deb6784a516ddca
Evgeny Motorin <zmotorin174@gmail.com>
2018-08-22 01:42:10 +0500

    add presentation activity

--- app/src/main/java/com/example/company/myapplication/AddPresentationActivity.kt
+++ app/src/main/java/com/example/company/myapplication/AddPresentationActivity.kt
@@ -1,8 +1,5 @@
 package com.example.company.myapplication
 
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_presentation.*
@@ -14,19 +11,12 @@ class AddPresentationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_presentation)
 
         TVpresentationText.text = intent.getStringExtra("presentation_txt")
-        editPresentationName.setOnFocusChangeListener { v, hasFocus ->
-            if (editPresentationName.text.toString() == "Enter the Name")
-                editPresentationName.setText("")
 
-            editPresentationName.setTextColor(Color.BLACK)
-            editPresentationName.setTypeface(Typeface.DEFAULT)
-        }
-
-        add_presentation_btn.setOnClickListener{
+       /* add_presentation_btn.setOnClickListener{
             val i = Intent(this, PresentationActivity::class.java)
             i.putExtra("presentation_txt", TVpresentationText.text.toString())
             i.putExtra("presentation_name",editPresentationName.text.toString())
             startActivity(i)
-        }
+        }*/
     }
 }
