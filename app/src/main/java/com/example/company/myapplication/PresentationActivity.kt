abf9376b35370764a43cef95ad5a46f1e943408a
Evgeny Motorin <zmotorin174@gmail.com>
2018-08-29 17:53:27 +0300

    added transition between activities

--- app/src/main/java/com/example/company/myapplication/PresentationActivity.kt
+++ app/src/main/java/com/example/company/myapplication/PresentationActivity.kt
@@ -1,7 +1,9 @@
 package com.example.company.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_presentation.*
 
 
 class PresentationActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class PresentationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_presentation)
+
+        trainingHistory.setOnClickListener {
+            startActivity(Intent(this,TrainingHistoryActivity::class.java))
+        }
+
+        training.setOnClickListener {
+            startActivity(Intent(this,TrainingActivity::class.java))
+        }
     }
 }
