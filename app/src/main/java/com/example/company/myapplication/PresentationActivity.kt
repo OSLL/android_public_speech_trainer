f9ff6e6d11fa89e4731d9b3f810149b7bf418e43
MikelDementyev <mike.new.steam@gmail.com>
2018-08-30 15:54:20 +0300

    task is done

--- app/src/main/java/com/example/company/myapplication/PresentationActivity.kt
+++ app/src/main/java/com/example/company/myapplication/PresentationActivity.kt
@@ -2,6 +2,7 @@ package com.example.company.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_presentation.*
 
 
 class PresentationActivity : AppCompatActivity() {
@@ -9,5 +10,8 @@ class PresentationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_presentation)
+
+        val name = intent.getStringExtra("presentation_name")
+        presentationName.text = name
     }
 }
