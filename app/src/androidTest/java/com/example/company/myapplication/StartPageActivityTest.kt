f63d8677d1deef9b64af458a7dbfe6bb252a4dac
MikelDementyev <mike.new.steam@gmail.com>
2018-08-30 16:30:49 +0300

    updated test

--- app/src/androidTest/java/com/example/company/myapplication/StartPageActivityTest.kt
+++ app/src/androidTest/java/com/example/company/myapplication/StartPageActivityTest.kt
@@ -1,28 +1,21 @@
 package com.example.company.myapplication
 
 import android.content.ComponentName
-import android.support.test.InstrumentationRegistry
 import android.support.test.InstrumentationRegistry.getInstrumentation
 import android.support.test.InstrumentationRegistry.getTargetContext
-import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.intent.Intents.intended
 import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import android.support.test.espresso.intent.rule.IntentsTestRule
-import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.runner.AndroidJUnit4
-
+import com.example.company.myapplication.R.string.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -30,7 +23,7 @@ import org.junit.Rule
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class StartPageActivityTest {
 
     @Rule
     @JvmField
@@ -39,35 +32,29 @@ class ExampleInstrumentedTest {
     @Test
     fun buttonsExist() {
         onView(withId(R.id.addBtn)).check(matches(isDisplayed()))
-        onView(withText("Презентация 1")).check(matches(isDisplayed()))
+        onView(withText(first_presentation)).check(matches(isDisplayed()))
         onView(withId(R.id.pres1)).check(matches(isDisplayed()))
-        onView(withText("Презентация 2")).check(matches(isDisplayed()))
+        onView(withText(second_presentation)).check(matches(isDisplayed()))
         onView(withId(R.id.pres2)).check(matches(isDisplayed()))
-        onView(withText("+")).check(matches(isDisplayed()))
+        onView(withText(add_presentation_symbol)).check(matches(isDisplayed()))
     }
 
     @Test
     fun test_from_start_page_to_presentation_with_pres1(){
-        onView(withId(R.id.pres1)).perform(ViewActions.click())
+        onView(withId(R.id.pres1)).perform(click())
         intended(hasComponent(ComponentName(getTargetContext(), PresentationActivity::class.java)))
     }
 
     @Test
     fun test_from_start_page_to_presentation_with_pres2(){
-        onView(withId(R.id.pres2)).perform(ViewActions.click())
+        onView(withId(R.id.pres2)).perform(click())
         intended(hasComponent(ComponentName(getTargetContext(), PresentationActivity::class.java)))
     }
-
-    @Test
-    fun test_from_start_page_to_create_presentation(){
-        onView(withId(R.id.addBtn)).perform(ViewActions.click())
-        intended(hasComponent(ComponentName(getTargetContext(), CreatePresentationActivity::class.java)))
-    }
-
+    
     @Test
     fun test_from_start_page_to_preference(){
         openActionBarOverflowOrOptionsMenu(getInstrumentation().targetContext)
-        onView(withText("Настройки")).perform(click());
+        onView(withText(activity_preference)).perform(click());
         intended(hasComponent(ComponentName(getTargetContext(), PreferenceActivity::class.java)))
     }
 }
