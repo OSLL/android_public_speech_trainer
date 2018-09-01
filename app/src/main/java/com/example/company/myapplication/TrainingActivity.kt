8582cb4c008a54f49ad71768480e752fe99f49d4
MikelDementyev <mike.new.steam@gmail.com>
2018-09-01 17:50:20 +0300

    task is done

--- app/src/main/java/com/example/company/myapplication/TrainingActivity.kt
+++ app/src/main/java/com/example/company/myapplication/TrainingActivity.kt
@@ -1,16 +1,22 @@
 package com.example.company.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.ParcelFileDescriptor
+import android.support.v7.app.AlertDialog
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_training.*
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class TrainingActivity : AppCompatActivity() {
 
@@ -18,16 +24,36 @@ class TrainingActivity : AppCompatActivity() {
     private var currentPage: PdfRenderer.Page? = null
     private var parcelFileDescriptor: ParcelFileDescriptor? = null
 
+    private var isCancelled = false
+
+    @SuppressLint("UseSparseArrays")
+    var dictionary = HashMap <Int, Long>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
+
+        var time = intent.getLongExtra("TrainingTime", 0)
 
         next.setOnClickListener {
             val index = currentPage?.index
             if(renderer != null && index != null) {
                 val NIndex: Int = index
                 renderPage(NIndex + 1)
+
+                val min = time_left.text.toString().substring(0, time_left.text.indexOf("m")-1)
+                val sec = time_left.text.toString().substring(time_left.text.indexOf(":") + 2,
+                        time_left.text.indexOf("s")-1)
+
+                time -= min.toLong()*60 + sec.toLong()
+                dictionary[index+1] = time
+
+                time = min.toLong()*60 + sec.toLong()
             }
+        }
+
+        finish.setOnClickListener{
+            timer(1,1).onFinish()
         }
     }
 
@@ -35,6 +61,58 @@ class TrainingActivity : AppCompatActivity() {
         super.onStart()
         initRenderer()
         renderPage(0)
+        val TrainingTime = intent.getLongExtra("TrainingTime", 0)
+        timer(TrainingTime*1000,1000).start()
+    }
+
+    private fun timer(millisInFuture:Long,countDownInterval:Long): CountDownTimer {
+        return object: CountDownTimer(millisInFuture,countDownInterval){
+
+            override fun onTick(millisUntilFinished: Long){
+                val timeRemaining = timeString(millisUntilFinished)
+                if (isCancelled){
+                    time_left.text = "Тренировка завершена"
+                    cancel()
+                }else{
+                    time_left.text = timeRemaining
+                }
+            }
+            override fun onFinish() {
+                isCancelled = true
+                timer(1,1).cancel()
+                val builder = AlertDialog.Builder(this@TrainingActivity)
+                builder.setMessage("Тренировка завершена")
+                builder.setPositiveButton("Статистика тренировки"){_,_->
+                    val stat = Intent(this@TrainingActivity, TrainingStatisticsActivity::class.java)
+                    startActivity(stat)
+                }
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+            }
+        }
+    }
+
+    @SuppressLint("UseSparseArrays")
+    private fun timeString(millisUntilFinished:Long):String{
+
+        var millisUntilFinishedVar:Long = millisUntilFinished
+
+/*
+        val hours = TimeUnit.MILLISECONDS.toHours(millisUntilFinished)
+        millisUntilFinished -= TimeUnit.HOURS.toMillis(hours)
+*/
+
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinishedVar)
+        millisUntilFinishedVar -= TimeUnit.MINUTES.toMillis(minutes)
+
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinishedVar)
+
+        // Format the string
+        return String.format(
+                Locale.getDefault(),
+                "%02d min: %02d sec",
+                minutes,seconds
+        )
     }
 
     private fun renderPage(pageIndex: Int){
@@ -80,7 +158,7 @@ class TrainingActivity : AppCompatActivity() {
 
             parcelFileDescriptor = ParcelFileDescriptor.open(temp, ParcelFileDescriptor.MODE_READ_ONLY)
             renderer = PdfRenderer(parcelFileDescriptor)
-        } catch(e: IOException){
+        } catch (e: IOException){
             Toast.makeText(this, "the exception happened", Toast.LENGTH_SHORT).show()
         }
     }
