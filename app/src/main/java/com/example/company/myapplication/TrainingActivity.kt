96978d24aa344d5bc16d1ad3154ed5bb8328500b
FlashyFire <rizotto@list.ru>
2018-09-05 23:08:04 +0300

    StopButton

--- app/src/main/java/com/example/company/myapplication/TrainingActivity.kt
+++ app/src/main/java/com/example/company/myapplication/TrainingActivity.kt
@@ -1,12 +1,40 @@
 package com.example.company.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_training.*
 
 class TrainingActivity : AppCompatActivity() {
+
+    var slideIndex = 1
+    var slideCount = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
+        next.isEnabled = true
+        finish.isEnabled = false
+
+        updateSlide()
+
+        next.setOnClickListener {
+            if (slideIndex < slideCount) {
+                slideIndex++
+                updateSlide()
+            }
+            if (slideIndex == slideCount){
+                next.isEnabled = false
+                finish.isEnabled = true
+            }
+        }
+
+        finish.setOnClickListener {
+            startActivity(Intent(this,TrainingStatisticsActivity::class.java))
+        }
+    }
+
+    private fun updateSlide() {
+        time_left.text = "$slideIndex / $slideCount / Осталось времени:"
     }
 }
