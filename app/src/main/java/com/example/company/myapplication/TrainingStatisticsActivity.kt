6b4a071e6283edecf1eb371621365150f55d9abd
Evgeny Motorin <zmotorin174@gmail.com>
2018-09-06 23:06:46 +0300

    fix chart

--- app/src/main/java/com/example/company/myapplication/TrainingStatisticsActivity.kt
+++ app/src/main/java/com/example/company/myapplication/TrainingStatisticsActivity.kt
@@ -2,6 +2,7 @@ package com.example.company.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
@@ -35,17 +36,28 @@ class TrainingStatisticsActivity : AppCompatActivity() {
         for(entry in lineEntries)
             labels.add(entry.x.toInt().toString())
 
-        val barDataSet = BarDataSet(lineEntries, "Количество слов в минуту")
+        val barDataSet = BarDataSet(lineEntries, getString(R.string.words_count))
         barDataSet.setColors(ColorTemplate.COLORFUL_COLORS,255)
 
         val data = BarData(barDataSet)
+        data.setValueTextSize(0f)
 
         speed_bar_chart.setFitBars(true)
         speed_bar_chart.setData(data)
-        speed_bar_chart.description.text = "Номер слайда"
-        speed_bar_chart.description.setTextSize(10f)
+        speed_bar_chart.description.text = getString(R.string.slide_number)
+        speed_bar_chart.description.setTextSize(15f)
         speed_bar_chart.animateXY(1000,1000)
-        speed_bar_chart.legend.setTextSize(12f)
+        speed_bar_chart.legend.setTextSize(20f)
+        speed_bar_chart.legend.setPosition(Legend.LegendPosition.ABOVE_CHART_LEFT)
+        speed_bar_chart.legend.formSize = 0f
+        speed_bar_chart.legend.xEntrySpace = 0f
+
+
+        speed_bar_chart.setScaleEnabled(false)//выкл возможность зумить
+        speed_bar_chart.getXAxis().setDrawGridLines(false)//отключение горизонтальных линии сетки
+        speed_bar_chart.axisRight.isEnabled = false// ось У справа невидимая
+        speed_bar_chart.axisLeft.setDrawGridLines(false)//откл вертикальных линий сетки
+        speed_bar_chart.axisLeft.textSize = 15f
 
         val xAxis = speed_bar_chart.xAxis
         xAxis.textSize = 12f
