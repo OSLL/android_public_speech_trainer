44d03530e0a9f3a713349a259725ffd0719d45d1
Evgeny Motorin <zmotorin174@gmail.com>
2018-10-17 03:39:55 +0300

    rm old recognizer, add new with service

--- app/src/main/java/com/example/company/myapplication/TrainingActivity.kt
+++ app/src/main/java/com/example/company/myapplication/TrainingActivity.kt
@@ -2,8 +2,11 @@ package com.example.company.myapplication
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Service
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
@@ -11,13 +14,7 @@ import android.media.MediaMetadataRetriever
 import android.media.MediaRecorder
 import android.media.AudioManager
 import android.net.Uri
-import android.os.Bundle
-import android.os.CountDownTimer
-import android.os.Environment
-import android.os.ParcelFileDescriptor
-import android.speech.RecognitionListener
-import android.speech.RecognizerIntent
-import android.speech.SpeechRecognizer
+import android.os.*
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
@@ -53,10 +50,17 @@ class TrainingActivity : AppCompatActivity() {
     @SuppressLint("UseSparseArrays")
     var TimePerSlide = HashMap<Int, Long>()
 
-    //private var PresentEntries = mutableMapOf<Int,Float?>()
+    //speech recognizer part
     private var PresentEntries = HashMap<Int,Float?>()
     private var curPageNum = 1
     private var curText = ""
+    private var mIntent: Intent? = null
+    private var myService: MyService? = null
+    internal var mBound = false
+    private var taskServiceAnswer: TaskServiceAnswer? = null
+    private var audioManager: AudioManager? = null
+    private var ALL_PRESENTATION_TEXT = "" //Текст для PIE CHART
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,126 +68,205 @@ class TrainingActivity : AppCompatActivity() {
 
         var time = intent.getLongExtra(TIME_ALLOTTED_FOR_TRAINING, 0)
 
-        initAudioRecording()
-        muteSound() // mute для того, чтобы не было слышно звуков speech recognizer
+        // Запись звука мешает работе speech recognizer, а именно mediaRecorder.start().
+        // SpeechRecognizer начинает бесконечно запускаться, не останавливая старые экземпляры,
+        // после mediaRecorder.stop() его работа нормализуется;
+        // error service - nullpointerexception
+        //initAudioRecording()
 
-        val mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
-        val mSpeechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
-                Locale.getDefault())
+        AddPermission()
 
-        mSpeechRecognizer.setRecognitionListener(object : RecognitionListener {
-            override fun onReadyForSpeech(bundle: Bundle) {
+        //finish.isEnabled = false
 
-            }
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        mIntent = Intent(this@TrainingActivity, MyService::class.java)
 
-            override fun onBeginningOfSpeech() {
+        startRecognizingService()
 
-            }
-
-            override fun onRmsChanged(v: Float) {
-
-            }
-
-            override fun onBufferReceived(bytes: ByteArray) {
-
-            }
-
-            override fun onEndOfSpeech() {
-
-            }
-
-            override fun onError(i: Int) {
-            }
-
-            override fun onResults(bundle: Bundle) {
-                val matches = bundle
-                        .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-
-                if (matches != null) {
-                    curText = matches[0]
-                }
-            }
-
-            override fun onPartialResults(bundle: Bundle) {
-
-            }
-
-            override fun onEvent(i: Int, bundle: Bundle) {
-
-            }
-        })
-
-        mSpeechRecognizer.startListening(mSpeechRecognizerIntent)
-
-        finish.isEnabled = false
 
         next.setOnClickListener {
+            next.isEnabled = false
+            audioManager!!.isMicrophoneMute = true
+            Toast.makeText(this, "Saving State, Please Wait", Toast.LENGTH_SHORT).show()
+
             val index = currentPage?.index
             if (renderer != null && index != null) {
-                val NIndex: Int = index
-                renderPage(NIndex + 1)
+                val handler = Handler()
+                handler.postDelayed({
+                    val NIndex: Int = index
+                    renderPage(NIndex + 1)
 
-                val min = time_left.text.toString().substring(0, time_left.text.indexOf("m") - 1)
-                val sec = time_left.text.toString().substring(
-                    time_left.text.indexOf(":") + 2,
-                    time_left.text.indexOf("s") - 1
-                )
+                    val min = time_left.text.toString().substring(0, time_left.text.indexOf("m") - 1)
+                    val sec = time_left.text.toString().substring(
+                            time_left.text.indexOf(":") + 2,
+                            time_left.text.indexOf("s") - 1
+                    )
 
-                time -= min.toLong() * 60 + sec.toLong()
-                TimePerSlide[index + 1] = time
+                    time -= min.toLong() * 60 + sec.toLong()
+                    TimePerSlide[index + 1] = time
 
-                time = min.toLong()*60 + sec.toLong()
+                    time = min.toLong()*60 + sec.toLong()
 
-                mSpeechRecognizer.stopListening()
-                mSpeechRecognizer.startListening(mSpeechRecognizerIntent)
 
-                val SlideReadSpeed: Float
-                if (curText == "")
-                    SlideReadSpeed = 0f
-                else
-                    SlideReadSpeed = curText.split(" ").size.toFloat() / TimePerSlide[curPageNum]!!.toFloat() * 60f
+                    val SlideReadSpeed: Float
+                    if (curText == "")
+                        SlideReadSpeed = 0f
+                    else
+                        SlideReadSpeed = curText.split(" ").size.toFloat() / TimePerSlide[curPageNum]!!.toFloat() * 60f
 
-                PresentEntries.put(curPageNum++,SlideReadSpeed)
+                    PresentEntries.put(curPageNum++,SlideReadSpeed)
 
-                Log.d("speechT", "NUMBER OF PAGE: " + (curPageNum-1).toString())
-                Log.d("speechT", "TEXT: " + curText)
-                Log.d("speechT", "read speed: " + PresentEntries.get(curPageNum-1).toString())
-                curText = ""
+                    Log.d("speechT", "NUMBER OF PAGE: " + (curPageNum-1).toString())
+                    Log.d("speechT", "TEXT: " + curText)
+                    Log.d("speechT", "read speed: " + PresentEntries.get(curPageNum-1).toString())
+                    Log.d("testServiceCT","CURRENT TEXT: " + curText)
+
+                    ALL_PRESENTATION_TEXT += " " + curText
+                    curText = ""
+                    myService!!.message = ""
+                    audioManager!!.isMicrophoneMute = false
+                }, 2000)
+
             }
         }
 
         finish.setOnClickListener{
-            if (!finishedRecording) {
+            /*if (!finishedRecording) {
                 stopAudioRecording()
                 finishedRecording = true
-            }
+            }*/
 
 
-            val SlideReadSpeed: Float
+            /*val SlideReadSpeed: Float
             if (curText == "")
                 SlideReadSpeed = 0f
             else
-                SlideReadSpeed = curText.split(" ").size.toFloat() / time!!.toFloat() * 60f
+                SlideReadSpeed = curText.split(" ").size.toFloat() / time.toFloat() * 60f
 
-            PresentEntries.put(curPageNum++,SlideReadSpeed)
+            PresentEntries.put(curPageNum++,SlideReadSpeed)*/
 
             timer(1,1).onFinish()
         }
     }
 
     //speech recognizer =====
+
     private fun AddPermission() {
         val permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
 
+        val arr = arrayOf(Manifest.permission.RECORD_AUDIO)
+
         if (permissionStatus != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO),
+            ActivityCompat.requestPermissions(this, arr,
                     1)
         }
     }
-    
+
+    private fun startRecognizingService(){
+        Log.d("testServiceCT","initrecognizer()")
+        audioManager!!.isMicrophoneMute = false
+        try {
+            taskServiceAnswer = TaskServiceAnswer()
+            taskServiceAnswer!!.execute()
+            myService!!.message = ""
+        } catch (e: NullPointerException) {
+            Log.d("testServiceEE", e.toString() + ", service status: " + taskServiceAnswer!!.status.toString())
+        }
+    }
+
+    private fun stopRecognizingService(waitForRecognitionComplete: Boolean){
+        if (!waitForRecognitionComplete) {
+            try {
+                taskServiceAnswer!!.setEXECUTE_FLAG(false)
+                taskServiceAnswer!!.cancel(false)
+            } catch (e: NullPointerException) {
+                Log.d("testServiceEE", e.toString() + ", service status: " + taskServiceAnswer!!.status.toString())
+            }
+        }
+        else {
+            val SlideReadSpeed: Float
+            SlideReadSpeed = if (curText == "") 0f else curText.split(" ").size.toFloat() / TimePerSlide[curPageNum]!!.toFloat() * 60f
+
+            PresentEntries.put(curPageNum++, SlideReadSpeed)
+
+            Log.d("speechT", "NUMBER OF PAGE: " + (curPageNum - 1).toString())
+            Log.d("speechT", "TEXT: " + curText)
+            Log.d("speechT", "read speed: " + PresentEntries.get(curPageNum - 1).toString())
+            Log.d("testServiceCT", "CURRENT TEXT: " + curText)
+
+            ALL_PRESENTATION_TEXT += " " + curText
+            curText = ""
+            myService!!.message = ""
+            audioManager!!.isMicrophoneMute = false
+
+            taskServiceAnswer!!.setEXECUTE_FLAG(false)
+            taskServiceAnswer!!.cancel(false)
+        }
+    }
+
+    private val mConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val binder = service as MyService.LocalBinder
+            myService = binder.service
+            Log.d("testServiceMCONNTEC", binder.service.toString())
+            mBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            mBound = false
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    inner class TaskServiceAnswer : AsyncTask<Void, Void, Void>() {
+        private var EXECUTE_FLAG = true
+
+        override fun onPreExecute() {
+            try {
+                bindService(mIntent, mConnection, Service.BIND_AUTO_CREATE)
+                EXECUTE_FLAG = true
+            } catch (e: Exception) {
+                Log.d("testService", e.toString() + " on pre execute error")
+            }
+
+        }
+
+        override fun onPostExecute(aVoid: Void?) {
+            if (mBound) {
+                unbindService(mConnection)
+                mBound = false
+            }
+        }
+
+        override fun onProgressUpdate(vararg values: Void) {
+            try {
+                //Log.d("testService", mService!!.message.toString())
+                curText = myService!!.message
+            } catch (e: Exception) {
+                Log.d("testService", e.toString() + " onprogressUpdate")
+            }
+
+        }
+
+        override fun doInBackground(vararg voids: Void): Void? {
+            while (EXECUTE_FLAG) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(150)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+                publishProgress()
+            }
+            this.onPostExecute(null)
+            return null
+        }
+
+        fun setEXECUTE_FLAG(EXECUTE_FLAG: Boolean) {
+            this.EXECUTE_FLAG = EXECUTE_FLAG
+        }
+    }
+
 
 //======================
 
@@ -215,6 +298,15 @@ class TrainingActivity : AppCompatActivity() {
             override fun onFinish() {
                 isCancelled = true
                 timer(1, 1).cancel()
+
+                next.isEnabled = false
+                audioManager!!.isMicrophoneMute = true
+                Toast.makeText(this@TrainingActivity, "Finish...", Toast.LENGTH_SHORT).show()
+                val handler = Handler()
+                handler.postDelayed({
+                    stopRecognizingService(true)
+                },2000)
+
                 val builder = AlertDialog.Builder(this@TrainingActivity)
                 builder.setMessage(R.string.training_completed)
                 builder.setPositiveButton(R.string.training_statistics) { _, _ ->
@@ -222,7 +314,6 @@ class TrainingActivity : AppCompatActivity() {
 
                     stat.putExtra(getString(R.string.presentationEntries), PresentEntries)
 
-                    unmuteSound()
                     startActivity(stat)
                 }
                 val dialog: AlertDialog = builder.create()
@@ -255,7 +346,6 @@ class TrainingActivity : AppCompatActivity() {
     }
 
     private fun renderPage(pageIndex: Int) {
-
         currentPage?.close()
 
         currentPage = renderer?.openPage(pageIndex)
@@ -272,7 +362,7 @@ class TrainingActivity : AppCompatActivity() {
             currentPage?.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
             slide.setImageBitmap(bitmap)
             next.isEnabled = NIndex + 1 < NPageCount
-            finish.isEnabled = !next.isEnabled
+            //finish.isEnabled = !next.isEnabled
         }
     }
 
@@ -422,24 +512,6 @@ class TrainingActivity : AppCompatActivity() {
         }
     }
 
-    private  fun muteSound(){
-        var amanager= getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, true)
-        amanager.setStreamMute(AudioManager.STREAM_ALARM, true)
-        amanager.setStreamMute(AudioManager.STREAM_MUSIC, true)
-        amanager.setStreamMute(AudioManager.STREAM_RING, true)
-        amanager.setStreamMute(AudioManager.STREAM_SYSTEM, true)
-    }
-
-    private fun unmuteSound(){
-        var amanager= getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, false)
-        amanager.setStreamMute(AudioManager.STREAM_ALARM, false)
-        amanager.setStreamMute(AudioManager.STREAM_MUSIC, false)
-        amanager.setStreamMute(AudioManager.STREAM_RING, false)
-        amanager.setStreamMute(AudioManager.STREAM_SYSTEM, false)
-    }
-
     override fun onPause() {
         if (isFinishing) {
             currentPage?.close()
@@ -454,4 +526,8 @@ class TrainingActivity : AppCompatActivity() {
         super.onPause()
     }
 
+    override fun onDestroy() {
+        stopRecognizingService(false)
+        super.onDestroy()
+    }
 }
