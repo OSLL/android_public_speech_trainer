ee8439e6801d455949c94d17aa01d1e58a538197
Evgeny Motorin <zmotorin174@gmail.com>
2018-11-06 05:08:59 +0300

    removed the lags when scrolling

--- app/src/main/java/com/example/company/myapplication/StartPageActivity.kt
+++ app/src/main/java/com/example/company/myapplication/StartPageActivity.kt
@@ -5,10 +5,15 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.pdf.PdfRenderer
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -22,6 +27,10 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.OnItemClickListener
 import com.xwray.groupie.ViewHolder
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 
 const val debugSlides = "making_presentation.pdf"   //Название презентации из ресурсов для отладочного режима
@@ -41,8 +50,11 @@ class StartPageActivity : AppCompatActivity() {
 
     private var adapter: GroupAdapter<ViewHolder>? = null
     private var listPresentationData: List<PresentationData>? = null
-
     private var presentationDataDao: PresentationDataDao? = null
+
+    private var renderer: PdfRenderer? = null
+    private var currentPage: PdfRenderer.Page? = null
+    private var parcelFileDescriptor: ParcelFileDescriptor? = null
 
     @SuppressLint("CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -98,7 +110,7 @@ class StartPageActivity : AppCompatActivity() {
         }
 
         for (presentation in listPresentationData!!) {
-            adapter?.add(PresentationStartpageRow(presentation,this@StartPageActivity))
+            adapter?.add(PresentationStartpageRow(presentation,getFirstSlideBitmap(presentation.stringUri,presentation.debugFlag),this@StartPageActivity))
         }
 
         recyclerview_startpage.adapter = adapter
@@ -146,5 +158,67 @@ class StartPageActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
+    }
+
+    private fun renderPage(pageIndex: Int): Bitmap? {
+
+        currentPage?.close()
+        currentPage = renderer?.openPage(pageIndex)
+        val width = currentPage?.width
+        val height = currentPage?.height
+        val index = currentPage?.index
+        val pageCount = renderer?.pageCount
+
+        if(width != null && height != null && index != null && pageCount != null) {
+            val NWidth: Int = width
+            val NHeight: Int = height
+            val bitmap: Bitmap = Bitmap.createBitmap(NWidth, NHeight, Bitmap.Config.ARGB_8888)
+            currentPage?.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+            return  bitmap
+        }
+        return null
+    }
+
+    private fun initRenderer(strUri: String, debugFlag: Int){
+        val uri = Uri.parse(strUri)
+
+        try{
+            val temp = File(cacheDir, "tempImage.pdf")
+            val fos = FileOutputStream(temp)
+            val isChecked = debugFlag == 1
+            val ins: InputStream
+            ins = if(!isChecked) {
+                try {
+                    val cr = contentResolver
+                    cr.openInputStream(uri)
+                }catch (e: Exception) {
+                    Log.d("test_row", "editPres cr:" + e.toString())
+                } as InputStream
+            } else {
+                assets.open(strUri)
+            }
+
+            val buffer = ByteArray(1024)
+
+            var readBytes = ins.read(buffer)
+            while(readBytes != -1){
+                fos.write(buffer, 0, readBytes)
+                readBytes = ins.read(buffer)
+            }
+
+            fos.close()
+            ins.close()
+
+            parcelFileDescriptor = ParcelFileDescriptor.open(temp, ParcelFileDescriptor.MODE_READ_ONLY)
+            renderer = PdfRenderer(parcelFileDescriptor)
+        } catch(e: IOException){
+            Toast.makeText(this, "error in opening presentation file", Toast.LENGTH_LONG).show()
+            Log.d("error","error in opening presentation file")
+        }
+    }
+
+    private fun getFirstSlideBitmap(strUri: String, debugFlag: Int): Bitmap? {
+        initRenderer(strUri,debugFlag)
+        return renderPage(0)
     }
 }
--- app/src/main/java/com/example/company/myapplication/views/PresentationStartpageRow.kt
+++ app/src/main/java/com/example/company/myapplication/views/PresentationStartpageRow.kt
@@ -29,14 +29,10 @@ import java.io.File
 import java.io.FileOutputStream
 
 
-class PresentationStartpageRow(private val presentation: PresentationData, private val ctx: Context): Item<ViewHolder>() {
+class PresentationStartpageRow(private val presentation: PresentationData,private val firstPageBitmap: Bitmap?, private val ctx: Context): Item<ViewHolder>() {
     companion object {
         const val activatedChangePresentationFlag = 1
     }
-
-    private var renderer: PdfRenderer? = null
-    private var currentPage: PdfRenderer.Page? = null
-    private var parcelFileDescriptor: ParcelFileDescriptor? = null
 
     var presentationId: Int? = null
 
@@ -48,7 +44,7 @@ class PresentationStartpageRow(private val presentation: PresentationData, priva
         viewHolder.itemView.name_presentation_start_page_row.text = presentation.name
         viewHolder.itemView.time_limit_presentation_start_page_row.text = getStringPresentationTimeLimit(presentation.timeLimit!!)
         viewHolder.itemView.page_count_presentation_start_page_row.text = getStringPresentationPageCount(presentation.pageCount)
-        viewHolder.itemView.image_view_presentation_start_page_row.setImageBitmap(getFirstSlideBitmap(presentation.stringUri))
+        viewHolder.itemView.image_view_presentation_start_page_row.setImageBitmap(firstPageBitmap)
 
         presentationId = presentation.id
 
@@ -89,67 +85,5 @@ class PresentationStartpageRow(private val presentation: PresentationData, priva
         val cases = arrayOf(2, 0, 1, 1, 1, 2)
 
         return titles[if (n % 100 in 5..19) 2 else cases[if (n % 10 < 5) n % 10 else 5]]
-    }
-
-    private fun renderPage(pageIndex: Int): Bitmap? {
-
-        currentPage?.close()
-        currentPage = renderer?.openPage(pageIndex)
-        val width = currentPage?.width
-        val height = currentPage?.height
-        val index = currentPage?.index
-        val pageCount = renderer?.pageCount
-
-        if(width != null && height != null && index != null && pageCount != null) {
-            val NWidth: Int = width
-            val NHeight: Int = height
-            val bitmap: Bitmap = Bitmap.createBitmap(NWidth, NHeight, Bitmap.Config.ARGB_8888)
-            currentPage?.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
-            return  bitmap
-        }
-        return null
-    }
-
-    private fun initRenderer(strUri: String){
-        val uri = Uri.parse(strUri)
-
-        try{
-            val temp = File(ctx.cacheDir, "tempImage.pdf")
-            val fos = FileOutputStream(temp)
-            val isChecked = presentation.debugFlag == 1
-            val ins: InputStream
-            ins = if(!isChecked) {
-                try {
-                    val cr = ctx.contentResolver
-                    cr.openInputStream(uri)
-                }catch (e: Exception) {
-                    Log.d("test_row", "editPres cr:" + e.toString())
-                } as InputStream
-            } else {
-                ctx.assets.open(strUri)
-            }
-
-            val buffer = ByteArray(1024)
-
-            var readBytes = ins.read(buffer)
-            while(readBytes != -1){
-                fos.write(buffer, 0, readBytes)
-                readBytes = ins.read(buffer)
-            }
-
-            fos.close()
-            ins.close()
-
-            parcelFileDescriptor = ParcelFileDescriptor.open(temp, ParcelFileDescriptor.MODE_READ_ONLY)
-            renderer = PdfRenderer(parcelFileDescriptor)
-        } catch(e: IOException){
-            Toast.makeText(ctx, "error in opening presentation file", Toast.LENGTH_LONG).show()
-            Log.d("error","error in opening presentation file")
-        }
-    }
-
-    private fun getFirstSlideBitmap(strUri: String): Bitmap? {
-        initRenderer(strUri)
-        return renderPage(0)
     }
 }
