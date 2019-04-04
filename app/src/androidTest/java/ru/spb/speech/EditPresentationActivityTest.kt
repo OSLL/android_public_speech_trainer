d18f8586fb20314d18d4b7a84cd1bd985ede509a
Kate_Yurut <kateyurut@gmail.com>
2019-04-04 17:50:13 +0300

    Добавлены в тест проверка корректности названия и продолжительности презентации

--- app/src/androidTest/java/ru/spb/speech/EditPresentationActivityTest.kt
+++ app/src/androidTest/java/ru/spb/speech/EditPresentationActivityTest.kt
@@ -4,8 +4,7 @@ import android.preference.PreferenceManager
 import android.support.test.InstrumentationRegistry
 import android.support.test.InstrumentationRegistry.getTargetContext
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.action.ViewActions.longClick
+import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.PickerActions
 import android.support.test.espresso.matcher.RootMatchers.isDialog
@@ -16,6 +15,16 @@ import android.support.test.uiautomator.UiDevice
 import org.junit.*
 import org.junit.runner.RunWith
 import java.lang.Thread.sleep
+import android.widget.NumberPicker
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.UiController
+import android.support.test.espresso.ViewAction
+import android.view.View
+import org.hamcrest.Matcher
+import android.support.test.espresso.matcher.ViewMatchers.withClassName
+import android.support.test.espresso.Espresso.onView
+import org.hamcrest.Matchers
+
 
 @RunWith(AndroidJUnit4::class)
 class EditPresentationActivityTest {
@@ -57,11 +66,63 @@ class EditPresentationActivityTest {
 
         // Изменение даты при редактировании
         onView(withText("2035-5-12")).perform(longClick())
-        onView(withText("Редактировать")).perform(click())
+        onView(withText(activityTestRule.activity.getString(R.string.edit))).perform(click())
         onView(withId(R.id.datePicker)).perform(PickerActions.setDate(2036, 5, 12))
         onView(withId(R.id.addPresentation)).perform(click())
         sleep(2000)
         onView(withText("2036-5-12")).check(matches(isDisplayed()))
     }
 
+    @Test
+    fun setNameOfPresentation() {
+        // Изменение названия презентации при добавлении
+        onView(withId(R.id.addBtn)).perform(click())
+        onView(withId(R.id.presentationName)).perform(replaceText(activityTestRule.activity.getString(R.string.first_debug_presentation_name)))
+        onView(withId(R.id.addPresentation)).perform(click())
+        sleep(2000)
+        onView(withText(activityTestRule.activity.getString(R.string.first_debug_presentation_name))).check(matches(isDisplayed()))
+
+        // Изменение названия презентации при редактировании
+        onView(withText(activityTestRule.activity.getString(R.string.first_debug_presentation_name))).perform(longClick())
+        onView(withText(activityTestRule.activity.getString(R.string.edit))).perform(click())
+        onView(withId(R.id.presentationName)).perform(replaceText(activityTestRule.activity.getString(R.string.second_debug_presentation_name)))
+        onView(withId(R.id.addPresentation)).perform(click())
+        sleep(2000)
+        onView(withText(activityTestRule.activity.getString(R.string.second_debug_presentation_name))).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun setDurationOfPresentation() {
+        // Изменение длительности презентации при добавлении
+        onView(withId(R.id.addBtn)).perform(click())
+        onView(withId(R.id.numberPicker1)).perform(setNumber(activityTestRule.activity.resources.getInteger(R.integer.one_minute)))
+        onView(withId(R.id.addPresentation)).perform(click())
+        sleep(2000)
+        onView(withText(activityTestRule.activity.getString(R.string.first_debug_time))).check(matches(isDisplayed()))
+
+        // Изменение длительности презентации при редактировании
+        onView(withText(activityTestRule.activity.getString(R.string.first_debug_time))).perform(longClick())
+        onView(withText(activityTestRule.activity.getString(R.string.edit))).perform(click())
+        onView(withId(R.id.numberPicker1)).perform(setNumber(activityTestRule.activity.resources.getInteger(R.integer.two_minutes)))
+        onView(withId(R.id.addPresentation)).perform(click())
+        sleep(2000)
+        onView(withText(activityTestRule.activity.getString(R.string.second_debug_time))).check(matches(isDisplayed()))
+    }
+
+    fun setNumber(number: Int): ViewAction {
+        return object : ViewAction {
+            override fun perform(uiController: UiController, view:View) {
+                val NumberPicker = view as NumberPicker
+                NumberPicker.value = number
+
+            }
+
+            override fun getDescription(): String {
+                return activityTestRule.activity.getString(R.string.set_number_into_NP)           }
+
+            override fun getConstraints(): Matcher <View> {
+                return ViewMatchers.isAssignableFrom(NumberPicker::class.java)
+            }
+        }
+    }
 }
