c620a43fc1756c0973884203ab19617c89a218fa
MikelDementyev <mike.new.steam@gmail.com>
2019-04-10 01:05:32 +0300

    corrected defects

--- app/src/androidTest/java/ru/spb/speech/SoundTrackValidationTest.kt
+++ app/src/androidTest/java/ru/spb/speech/SoundTrackValidationTest.kt
@@ -40,22 +40,6 @@ class SoundTrackValidationTest {
         debSl.apply()
     }
 
-    @After
-    fun after(){
-        mDevice!!.pressBack()
-
-        onView(withText(presName)).perform(longClick())
-        onView(withText(mIntentsTestRule.activity.getString(R.string.remove))).perform(click())
-
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext())
-        val debSl = sharedPreferences.edit()
-        val onMode = mIntentsTestRule.activity.getString(R.string.deb_pres)
-        val onAudio = mIntentsTestRule.activity.getString(R.string.deb_speech_audio_key)
-        debSl.putBoolean(onMode, false)
-        debSl.putBoolean(onAudio, false)
-        debSl.apply()
-    }
-
     @Rule
     @JvmField
     var mIntentsTestRule = ActivityTestRule<StartPageActivity>(StartPageActivity::class.java)
@@ -79,6 +63,21 @@ class SoundTrackValidationTest {
         mDevice!!.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.training_statistics))).click()
 
         assertEquals(speed_statistics!!.toFloat(),mIntentsTestRule.activity.resources.getDimension(R.dimen.expected_number_of_recognized_words),mIntentsTestRule.activity.resources.getDimension(R.dimen.error_in_the_number_of_recognized_words))
+
+        sleep(mIntentsTestRule.activity.resources.getInteger(R.integer.time_in_milliseconds_wait_after_test).toLong())
+
+        mDevice!!.pressBack()
+
+        onView(withText(presName)).perform(longClick())
+        onView(withText(mIntentsTestRule.activity.getString(R.string.remove))).perform(click())
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext())
+        val debSl = sharedPreferences.edit()
+        val onMode = mIntentsTestRule.activity.getString(R.string.deb_pres)
+        val onAudio = mIntentsTestRule.activity.getString(R.string.deb_speech_audio_key)
+        debSl.putBoolean(onMode, false)
+        debSl.putBoolean(onAudio, false)
+        debSl.apply()
     }
 
 }
