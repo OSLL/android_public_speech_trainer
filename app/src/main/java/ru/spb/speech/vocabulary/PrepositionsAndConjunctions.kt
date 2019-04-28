e46698a3ac37a8806f964754557583f9a3af74a7
Evgeny Motorin <zmotorin174@gmail.com>
2019-04-28 16:22:16 +0300

    added custom stemmer

--- app/src/main/java/ru/spb/speech/vocabulary/PrepositionsAndConjunctions.kt
+++ app/src/main/java/ru/spb/speech/vocabulary/PrepositionsAndConjunctions.kt
@@ -1,11 +1,13 @@
 package ru.spb.speech.vocabulary
 
 import android.content.Context
+import android.util.Log
 import ru.spb.speech.R
 import java.util.regex.Pattern
 
 
 class PrepositionsAndConjunctions(private val ctx: Context) {
+    private val stemmer = StemmerPorterRU()
 
     fun removeConjunctionsAndPrepositionsFromText(sourceText: String): String {
         val list = ctx.resources.getStringArray(R.array.prepositionsAndConjunctions)
@@ -29,6 +31,12 @@ class PrepositionsAndConjunctions(private val ctx: Context) {
         }
 
         m.appendTail(sb)
-        return sb.toString()
+
+        var res = ""
+        for (w in sb.toString().split(" "))
+            res += " ${stemmer.stem(w)}"
+
+        Log.d("sabdasb", res)
+        return res
     }
 }
