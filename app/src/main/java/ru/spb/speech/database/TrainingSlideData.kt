5a9b590b169dbb6a5e7e8a81bb1ddcc8ddc7b24e
Evgeny Motorin <zmotorin174@gmail.com>
2019-05-21 15:04:35 +0300

    update db table

--- app/src/main/java/ru/spb/speech/database/TrainingSlideData.kt
+++ app/src/main/java/ru/spb/speech/database/TrainingSlideData.kt
@@ -6,7 +6,10 @@ import android.arch.persistence.room.*
 data class TrainingSlideData(@PrimaryKey(autoGenerate = true) var id:Int?,
                              @ColumnInfo(name = "spentTimeInSec") var spentTimeInSec: Long?,
                              @ColumnInfo(name = "knownWords") var knownWords: String?,
-                             @ColumnInfo(name = "nextSlideId") var nextSlideId : Int?
+                             @ColumnInfo(name = "nextSlideId") var nextSlideId : Int?,
+                             @ColumnInfo(name = "silencePercentage") var silencePercentage: Double?,
+                             @ColumnInfo(name = "pauseAverageLength") var pauseAverageLength: Int?,
+                             @ColumnInfo(name = "longPausesAmount") var longPausesAmount: Int?
 ){
-    constructor():this(null,0,null,null)
+    constructor():this(null,0,null,null, null, null, null)
 }
