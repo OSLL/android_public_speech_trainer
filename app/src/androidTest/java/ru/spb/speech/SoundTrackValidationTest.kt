44e8d17de71c1fc88043cf9f574fba67a3309543
MikelDementyev <mike.new.steam@gmail.com>
2019-05-24 15:10:06 +0300

    Обход диалога при старте приложения

--- app/src/androidTest/java/ru/spb/speech/SoundTrackValidationTest.kt
+++ app/src/androidTest/java/ru/spb/speech/SoundTrackValidationTest.kt
@@ -1,8 +1,6 @@
 package ru.spb.speech
 
-import android.preference.PreferenceManager
 import android.support.test.InstrumentationRegistry
-import android.support.test.InstrumentationRegistry.getTargetContext
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.matcher.ViewMatchers.*
@@ -11,12 +9,12 @@ import android.support.test.runner.AndroidJUnit4
 import android.support.test.uiautomator.UiDevice
 import android.support.test.uiautomator.UiSelector
 import junit.framework.Assert.assertEquals
-import org.hamcrest.CoreMatchers
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import ru.spb.speech.DBTables.SpeechDataBase
+import ru.spb.speech.database.SpeechDataBase
 import java.lang.Thread.sleep
 
 @RunWith(AndroidJUnit4::class)
@@ -29,8 +27,8 @@ class SoundTrackValidationTest : BaseInstrumentedTest() {
     @JvmField
     var mIntentsTestRule = ActivityTestRule<StartPageActivity>(StartPageActivity::class.java)
 
-    private var mDevice: UiDevice? = null
-    private var presName = ""
+    lateinit var helper : TestHelper
+    private lateinit var mDevice: UiDevice
     private val tContext = InstrumentationRegistry.getTargetContext()
     private val db: SpeechDataBase = SpeechDataBase.getInstance(tContext)!!
 
@@ -41,33 +39,26 @@ class SoundTrackValidationTest : BaseInstrumentedTest() {
     @Before
     fun before() {
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-        assertThat(mDevice, CoreMatchers.notNullValue())
+        helper = TestHelper(mIntentsTestRule.activity)
+        helper.setTrainingPresentationMod(true) // включение тестовой презентации
+    }
 
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext())
-        val debSl = sharedPreferences.edit()
-        val onMode = mIntentsTestRule.activity.getString(R.string.deb_pres)
-        val onAudio = mIntentsTestRule.activity.getString(R.string.deb_speech_audio_key)
-
-        debSl.putBoolean(onMode, true)
-        debSl.putBoolean(onAudio, true)
-        debSl.putBoolean(mIntentsTestRule.activity.getString(R.string.useStatistics), true)
-
-        debSl.apply()
+    @After
+    fun disableDebugMode() {
+        helper.setTrainingPresentationMod(false) // выключение тестовой презентации
+        helper.removeDebugSlides()
     }
 
     @Test
     fun soundTrackValidationTest(){
-
         onView(withId(R.id.addBtn)).perform(click())
-        presName = mIntentsTestRule.activity.getString(R.string.deb_pres_name).substring (0, mIntentsTestRule.activity.getString(R.string.deb_pres_name).indexOf(mIntentsTestRule.activity.getString(R.string.pdf_format)))
-        onView(withId(R.id.presentationName)).perform(clearText(), typeText(presName), closeSoftKeyboard())
+        onView(withId(R.id.presentationName)).perform(replaceText(mIntentsTestRule.activity.getString(R.string.making_presentation)))
         onView(withId(R.id.addPresentation)).perform(click())
-
-        mDevice!!.findObject(UiSelector().text(presName)).click()
+        helper.startTrainingDialog(mDevice)
 
         sleep(mIntentsTestRule.activity.resources.getInteger(R.integer.required_time_in_milliseconds_allotted_for_training).toLong())
 
-        mDevice!!.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.stop))).click()
+        mDevice.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.stop))).click()
 
         sleep(mIntentsTestRule.activity.resources.getInteger(R.integer.time_in_milliseconds_until_you_can_switch_to_workout_statistics).toLong())
 
@@ -77,18 +68,6 @@ class SoundTrackValidationTest : BaseInstrumentedTest() {
 
         sleep(mIntentsTestRule.activity.resources.getInteger(R.integer.time_in_milliseconds_wait_after_test).toLong())
 
-        mDevice!!.pressBack()
-
-        onView(withText(presName)).perform(longClick())
-        onView(withText(mIntentsTestRule.activity.getString(R.string.remove))).perform(click())
-
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext())
-        val debSl = sharedPreferences.edit()
-        val onMode = mIntentsTestRule.activity.getString(R.string.deb_pres)
-        val onAudio = mIntentsTestRule.activity.getString(R.string.deb_speech_audio_key)
-        debSl.putBoolean(onMode, false)
-        debSl.putBoolean(onAudio, false)
-        debSl.apply()
+        mDevice.pressBack()
     }
-
 }
