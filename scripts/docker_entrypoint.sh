757a1464b3d52de9d339e9f9108a049019b8cd99
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2019-08-15 11:17:31 +0300

    Update docker_entrypoint.sh

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -1,7 +1,7 @@
 ls -la ./
 ls -la app/
-echo $USER
-chown -R $USER:$USER ./
+whoami
+chown -R `whoami`:`whoami` ./
 mv "app/google-services-debug.json" "app/google-services.json"
 ./gradlew assembleDebug --stacktrace
 # ./gradlew assembleAndroidTest
