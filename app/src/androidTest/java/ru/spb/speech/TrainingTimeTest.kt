230eb2665897e50426d2a9904cf5a73919e7b916
Igor <villain.sergeev@mail.ru>
2019-08-31 15:28:57 +0300

    Исправлен тест

--- app/src/androidTest/java/ru/spb/speech/TrainingTimeTest.kt
+++ app/src/androidTest/java/ru/spb/speech/TrainingTimeTest.kt
@@ -1,15 +1,9 @@
 package ru.spb.speech
 
-import android.preference.PreferenceManager
 import android.support.test.InstrumentationRegistry
-import android.support.test.InstrumentationRegistry.getTargetContext
-import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions
-import android.support.test.espresso.assertion.ViewAssertions
-import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.intent.rule.IntentsTestRule
-import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.runner.AndroidJUnit4
 import android.support.test.uiautomator.UiDevice
@@ -17,13 +11,14 @@ import android.support.test.uiautomator.UiSelector
 import android.util.Log
 import ru.spb.speech.database.SpeechDataBase
 import junit.framework.Assert.assertEquals
-import org.hamcrest.CoreMatchers.containsString
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.math.abs
+import ru.spb.speech.appSupport.TrainingStatisticsData
+import ru.spb.speech.database.helpers.TrainingDBHelper
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class TrainingTimeTest : BaseInstrumentedTest() {
@@ -31,12 +26,21 @@ class TrainingTimeTest : BaseInstrumentedTest() {
     @JvmField
     var mIntentsTestRule = IntentsTestRule<StartPageActivity>(StartPageActivity::class.java)
 
+    val SLIDES_COUNT_IN_DEB_PRES = 26L
+    val TIME_PER_SLIDE = 6000L
+
+    val TRAINING_TIME_FULL = SLIDES_COUNT_IN_DEB_PRES * TIME_PER_SLIDE
+    val TRAINING_TIME_EATLY_STOP = 15000L
+
+    val DELTA = 0.1
+
     lateinit var helper: TestHelper
-    lateinit var uiDevice: UiDevice
+    lateinit var device: UiDevice
+    lateinit var presentationName: String
 
     @Before
     fun enableDebugMode() {
-        uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
         helper = TestHelper(mIntentsTestRule.activity)
         helper.setTrainingPresentationMod(true) // включение тестовой презентации
     }
@@ -48,60 +52,53 @@ class TrainingTimeTest : BaseInstrumentedTest() {
     }
 
     @Test
-    fun FullTrainTest(){
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext())
+    fun timerPresEnded() {
 
         onView(withId(R.id.addBtn)).perform(ViewActions.click())
         onView(withId(R.id.presentationName)).perform(ViewActions.replaceText(mIntentsTestRule.activity.getString(R.string.making_presentation)))
         onView(withId(R.id.addPresentation)).perform(ViewActions.click())
         Thread.sleep(2000)
-        helper.startTrainingDialog(uiDevice)
+        helper.startTrainingDialog(device)
 
-        val startTime = System.nanoTime()
-
-        for (i in 1..26) {
-            Thread.sleep(6000)
-            uiDevice.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.next))).click()
+        for (i in 1..SLIDES_COUNT_IN_DEB_PRES) {
+            Thread.sleep(TIME_PER_SLIDE)
+            device.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.next))).click()
         }
 
-        uiDevice.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.stop))).click()
+        device.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.stop))).clickAndWaitForNewWindow()
+        device.pressBack()
+        device.pressBack()
 
-        val estimatedTime = System.nanoTime() - startTime
-        onView(withId(android.R.id.button1)).perform(ViewActions.click())
-
-        val realTime = sharedPreferences.getInt(mIntentsTestRule.activity.getString(R.string.training_duration), -1).toLong()
-
-        if (abs(realTime - estimatedTime) > 5){
-            assertEquals(realTime, estimatedTime)
-        }
-
-
-
+        assertEquals(getTrainingTimeInMillis()!!.toFloat(), TRAINING_TIME_FULL.toFloat(), (TRAINING_TIME_FULL * DELTA).toFloat())
     }
 
     @Test
-    fun earlyEndTraining(){
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext())
+    fun timerEarlyStop() {
 
         onView(withId(R.id.addBtn)).perform(ViewActions.click())
         onView(withId(R.id.presentationName)).perform(ViewActions.replaceText(mIntentsTestRule.activity.getString(R.string.making_presentation)))
         onView(withId(R.id.addPresentation)).perform(ViewActions.click())
         Thread.sleep(2000)
-        helper.startTrainingDialog(uiDevice)
+        helper.startTrainingDialog(device)
 
-        val startTime = System.nanoTime()
-        Thread.sleep(6000)
-        uiDevice.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.stop))).click()
+        Thread.sleep(TRAINING_TIME_EATLY_STOP / 2)
 
-        val estimatedTime = System.nanoTime() - startTime
+        device.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.next))).click()
 
-        onView(withId(android.R.id.button1)).perform(ViewActions.click())
+        Thread.sleep(TRAINING_TIME_EATLY_STOP / 2)
 
-        val realTime = sharedPreferences.getInt(mIntentsTestRule.activity.getString(R.string.training_duration), -1).toLong()
+        device.findObject(UiSelector().text(mIntentsTestRule.activity.getString(R.string.stop))).clickAndWaitForNewWindow()
+        device.pressBack()
+        device.pressBack()
 
-        if (abs(realTime - estimatedTime) > 5){
-            assertEquals(realTime, estimatedTime)
-        }
+        assertEquals(getTrainingTimeInMillis()!!.toFloat(), TRAINING_TIME_EATLY_STOP.toFloat(), (TRAINING_TIME_EATLY_STOP * DELTA).toFloat())
+    }
+
+    private fun getTrainingTimeInMillis(): Long? {
+        val lastPresentation = SpeechDataBase.getInstance(mIntentsTestRule.activity)?.PresentationDataDao()?.getLastPresentation()
+        val lastTraining = TrainingDBHelper(mIntentsTestRule.activity).getAllTrainingsForPresentation(lastPresentation!!)!!.last()
+        val trainingTime = TrainingStatisticsData(mIntentsTestRule.activity, lastPresentation, lastTraining).currentTrainingTime
+        return TimeUnit.SECONDS.toMillis(trainingTime)
     }
 
 }
