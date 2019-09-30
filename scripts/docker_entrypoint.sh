9db871374ab6dca8bec69c0fe766019286fbe47d
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2019-09-30 17:05:45 +0300

    added additional checks

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -3,6 +3,7 @@ ls -la app/
 whoami
 chown -R `whoami`:`whoami` ./
 mv "app/google-services-debug.json" "app/google-services.json"
+./gradlew lintDebug
 ./gradlew assembleDebug --stacktrace
-# ./gradlew assembleAndroidTest
-# ./gradlew lintDebug
+./gradlew assembleAndroidTest
+./gradlew clean test --info
